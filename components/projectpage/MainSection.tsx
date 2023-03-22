909d7824de305ef42893826265ae89759209941f
rodrfentanes21 <rfentanes@sga.pucminas.br>
2023-03-22 15:29:15 -0300

    grammar

--- components/projectpage/MainSection.tsx
+++ components/projectpage/MainSection.tsx
@@ -6,7 +6,7 @@ export default function MainSection() {
   return (
     <section className="w-screen lg:w-full flex flex-col lg:flex-row justify-center items-center lg:h-screen px-5 lg:px-20 gap-5">
       <div className="flex flex-col justify-center h-3/4 w-full lg:w-1/2 gap-5">
-        <h2 className="text-3xl text-dodgerblue-100">Os Desenvolvedores</h2>
+        <h2 className="text-3xl text-dodgerblue-100">Os Criadores</h2>
         <p className="text-justify">
           (texto sobre pipi popopo alunos puc minas bla bla bla) Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi quasi
           esse dolorem doloremque, delectus nisi odio ea tempore vitae commodi
@@ -21,8 +21,8 @@ export default function MainSection() {
         </p>
       </div>
       {/* TODO resolver o object da imagem mas fiquei sem net no meio da  aula */}
-      <div className="h-3/4 w-full lg:w-1/2 grid place-items-center object-cover select-none rounded-bl-3xl rounded-tr-3xl">
-        <Image src={PUCMinas} alt="puc minas photo" placeholder="blur" width={1000} height={1000} style={{objectFit:"cover"}}/>
+      <div className="w-[20rem] h-[20rem] lg:w-1/2 grid place-items-center select-none rounded-bl-3xl rounded-tr-3xl">
+        <Image src={PUCMinas} alt="puc minas photo" placeholder="blur" width={1000} height={1000} style={{objectFit:"contain"}}/>
       </div>
     </section>
   );
