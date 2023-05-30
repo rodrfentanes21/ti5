05ce42443d28f8e02dea9c3e5381198bc4b3005b
Rodrigo Fentanes <rfentanes@sga.pucminas.br>
2023-05-30 19:52:13 -0300

    garuda unfinished

--- src/pages/documentacao/garuda.tsx
+++ src/pages/documentacao/garuda.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "../../../components/mainpage/Header";
 import DocsNavBar from "../../../components/documentationpage/DocsNavBar";
+import Link from "next/link";
 
 export default function documentacao() {
   return (
@@ -8,9 +9,30 @@ export default function documentacao() {
       <Header altUrl={true} />
       <div className="w-screen h-screen flex justify-center items-center p-20">
         <div className="flex justify-start items-center w-full h-full  rounded-2xl bg-slate-200">
-        <DocsNavBar />
+          <DocsNavBar />
           <div className="grow h-full flex justify-center items-center">
-            garuda
+            <div className="self-center w-4/5 h-4/5 flex flex-col gap-3 py-3 item-center justify-start">
+              <h1 className="text-2xl bold">Garuda</h1>
+              <p>
+                texto texto
+                <br />
+                Para mais informações, visite a
+                <Link
+                  href="https://garudalinux.org/downloads.html"
+                  target="_blank"
+                  className="text-dodgerblue-100"
+                >
+                  {" "}
+                  página principal
+                </Link>
+              </p>
+              <h1 className="text-2xl bold">Especificações Recomendadas:</h1>
+              <ul>
+                <li>4GB RAM</li>
+                <li>30GB de Espaço em Disco</li>
+                <li>Sistema de 64 bits</li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
